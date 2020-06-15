ad019ed8d8ed1a224f66e4ddbc0052a5c11f954c
erickubenka <ek@code-fever.de>
2020-06-16 01:02:56 +0200

    Fixed #9, Fixes #8 by adding latest dependencies, updated readme, as well as simple java test

---
+++ gladini-test/src/test/java/de/codefever/gladini/SimpleSeleniumTest.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2020 Eric Kubenka.
+ * Licensed under the MIT License.
+ * See LICENSE file in the project root for full license information.
+ */
+package de.codefever.gladini;
+
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * @author Eric Kubenka
+ * creation date: 16.06.2020 - 00:52
+ */
+public class SimpleSeleniumTest {
+
+    private void nap() {
+        try {
+            long timeout = (long) (Math.random() * 10) * 1000;
+            System.out.println("nap for " + timeout);
+            Thread.sleep(timeout);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testT01_Start() throws MalformedURLException, InterruptedException {
+
+        nap();
+        final RemoteWebDriver driver = new RemoteWebDriver(new URL("http://localhost:30020"), new ChromeOptions());
+        System.out.println("Session id is: " + driver.getSessionId());
+        driver.get("https://google.de");
+
+        for (int i = 0; i < 30; i++) {
+            Thread.sleep(1000);
+            Assert.assertEquals(driver.getTitle(), "Google");
+        }
+
+        driver.quit();
+    }
+
+    @Test
+    public void testT02_Start() throws MalformedURLException {
+
+        nap();
+        final RemoteWebDriver driver = new RemoteWebDriver(new URL("http://localhost:30020"), new ChromeOptions());
+        System.out.println("Session id is: " + driver.getSessionId());
+        driver.get("https://google.de");
+        nap();
+        Assert.assertEquals(driver.getTitle(), "Google");
+        driver.quit();
+    }
+
+    @Test
+    public void testT03_Start() throws MalformedURLException {
+
+        final RemoteWebDriver driver = new RemoteWebDriver(new URL("http://localhost:30020"), new ChromeOptions());
+        System.out.println("Session id is: " + driver.getSessionId());
+        driver.get("https://google.de");
+        nap();
+        Assert.assertEquals(driver.getTitle(), "Google");
+        driver.quit();
+    }
+
+}
